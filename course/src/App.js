986ffe270d3a5d88d817294285adee1fde4f5b26
Shudh25 <pandey252001@gmail.com>
2023-12-12 22:51:30 +0530

    creating biorithmic calculator

--- course/src/App.js
+++ course/src/App.js
@@ -1,50 +1,17 @@
 import React from 'react';
-import { IonApp, IonButton, IonContent, IonHeader, IonIcon, IonTitle, IonToolbar, setupIonicReact, useIonToast } from '@ionic/react';
-import { play as playIcon } from 'ionicons/icons';
 
-/* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
-/* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
-/* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-// import '@ionic/react/css/float-elements.css';
-// import '@ionic/react/css/text-alignment.css';
-// import '@ionic/react/css/text-transformation.css';
-// import '@ionic/react/css/flex-utils.css';
-// import '@ionic/react/css/display.css';
-
-/* Custom styles */
-// import './theme/variables.css';
-
-setupIonicReact();
+import { IonApp, IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
 
 function App() {
-  const [presentToast] = useIonToast();
-
-  const handleClick = () => {
-    presentToast({
-      message: 'Hello world!',
-      duration: 1500,
-    });
-  };
-
   return (
     <IonApp>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Learning UI</IonTitle>
+          <IonTitle>My App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        <IonButton onClick={handleClick}>
-          <IonIcon icon={playIcon} slot="start" />
-          Click Me
-        </IonButton>
+        <p>Add some content here…</p>
       </IonContent>
     </IonApp>
   );
